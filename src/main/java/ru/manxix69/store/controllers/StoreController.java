2a130244f46eab0fccee2c94dbb576b73928c727
manxix69 <manxix69@gmail.com>
2024-04-09 00:13:02 +0500

    Initial commit

---
+++ src/main/java/ru/manxix69/store/controllers/StoreController.java
@@ -0,0 +1,18 @@
+package ru.manxix69.store.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.manxix69.store.services.StoreService;
+
+@RestController
+@RequestMapping("/store/order")
+public class StoreController {
+    private final StoreService storeService;
+
+    public StoreController(StoreService storeService) {
+        this.storeService = storeService;
+    }
+
+}
---
+++ src/main/java/ru/manxix69/store/services/StoreService.java
@@ -0,0 +1,8 @@
+package ru.manxix69.store.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface StoreService {
+
+}
